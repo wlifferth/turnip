d0816356e313a3d976d88678bda7257836a6afc0
wlifferth <will.lifferth@gmail.com>
2021-03-07 12:54:08 -0800

    refactor to make index.html static and cleanup insert_new_message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import collections
 import datetime
 import json
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='')
 
 # We'll store the most recent 10 messages
 messages = collections.deque(maxlen=100)
@@ -12,21 +12,25 @@ messages = collections.deque(maxlen=100)
 def index():
     if flask.request.method == 'POST':
         request_ip_address = get_client_ip_address()
-        insert_new_message({
-            'text': flask.request.form['message'],
-            'username': get_username(request_ip_address),
-            'creation_time': datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
-            'color': get_color(request_ip_address)
-        })
-    return flask.render_template('index.html', messages=get_recent_messages())
+        insert_new_message(
+            text=flask.request.form['message'],
+            username=get_username(request_ip_address),
+            color=get_color(request_ip_address)
+        )
+    return flask.send_file('index.html')
 
 @app.route('/messages', methods = ['GET'])
 def message_data():
     return json.dumps(get_recent_messages())
 
 # Functions for changing state
-def insert_new_message(message):
-    messages.append(message)
+def insert_new_message(username, color, text):
+    messages.append({
+        'text': flask.request.form['message'],
+        'username': get_username(request_ip_address),
+        'color': get_color(request_ip_address),
+        'creation_time': datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
+    })
 
 def get_recent_messages(num_messages=100):
     messages_list = list(reversed(messages))
